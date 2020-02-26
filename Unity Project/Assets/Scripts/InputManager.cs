6ae710ce599e3d0860edcc6d477153c5845fed31
Nagisa-Ogawa <54796956+Nagisa-Ogawa@users.noreply.github.com>
2020-02-26 15:32:47 +0900

    [add]add_KeyIn&add_PlayerRun (#1)
    ・プレイヤーのキー入力処理を追加しました
    ・プレイヤーの移動処理を追加しました

---
+++ Unity Project/Assets/Scripts/InputManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    /// <summary>
+    /// ジャンプのキー入力を受け取る関数
+    /// </summary>
+    /// <param name="playerNum"></param>
+    /// <returns></returns>
+    public bool JumpKeyIn(int playerNum)
+    {
+        if (Input.GetButtonDown("player" + playerNum + "_jump"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    /// <summary>
+    /// アクションボタンのキー入力を受け取る関数
+    /// </summary>
+    /// <param name="playerNum"></param>
+    /// <returns></returns>
+    public bool ActionKeyIn(int playerNum)
+    {
+        if (Input.GetButtonDown("player" + playerNum + "_action"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    /// <summary>
+    /// ショットのキー入力を受け取る関数
+    /// </summary>
+    /// <param name="playerNum"></param>
+    /// <returns></returns>
+    public bool ShotKeyIn(int playerNum)
+    {
+        float tri = Input.GetAxis("player" + playerNum + "_shot");
+
+        if(tri>0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    /// <summary>
+    /// 滑空開始のキー入力を受け取る関数
+    /// </summary>
+    /// <param name="playerNum"></param>
+    /// <returns></returns>
+    public bool StartGlidingKeyIn(int playerNum)
+    {
+        if(Input.GetButtonDown("player" + playerNum + "_jump"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    /// <summary>
+    /// 滑空終了のキー入力を受け取る関数
+    /// </summary>
+    /// <param name="playerNum"></param>
+    /// <returns></returns>
+    public bool EndGlidingKeyIn(int playerNum)
+    {
+        if (Input.GetButtonUp("player" + playerNum + "_jump"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+
+    private void Update()
+    {
+        if(JumpKeyIn(1))
+        {
+            Debug.Log("プレイヤー１のジャンプ");
+        }
+        if(ActionKeyIn(1))
+        {
+            Debug.Log("プレイヤー１のアクション");
+        }
+        if(ShotKeyIn(1))
+        {
+            Debug.Log("プレイヤー１のショット");
+        }
+    }
+
+}
---
+++ Unity Project/Assets/Scripts/PlayerRun.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRun : MonoBehaviour
+{
+    // 移動速度
+    [SerializeField]
+    float speed;
+
+    Rigidbody2D rigidbody2d;
+
+    private void Start()
+    {
+        // rigidbodyをセット
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+    }
+
+
+    /// <summary>
+    /// プレイヤーの移動処理
+    /// </summary>
+    public void Run()
+    {
+        // 移動ベクトル
+        Vector2 moveVec;
+        moveVec = Vector2.right * speed;
+        rigidbody2d.velocity = new Vector2(moveVec.x, rigidbody2d.velocity.y);
+    }
+
+
+    /// <summary>
+    /// プレイヤーの移動速度をセットする関数
+    /// </summary>
+    /// <param name="speed"></param>
+    public void SetSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+
+}
