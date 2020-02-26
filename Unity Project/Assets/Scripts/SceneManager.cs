2598d8448d50992a6b6324013e2fff46a5576f28
Nagisa-Ogawa <54796956+Nagisa-Ogawa@users.noreply.github.com>
2020-02-26 16:51:54 +0900

    [add]add_SceneManager (#3)
    ・SceneManager.csを追加しました

---
+++ Unity Project/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+
+    #region シングルトン
+    // シングルトン
+    private static SceneManager instance;
+    public static SceneManager Instance
+    {
+        get { return instance; }
+    }
+
+    private void Awake()
+    {
+        // 複数個作成しないようにする
+        if(instance==null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Ready());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    /// <summary>
+    /// ゲーム開始前の処理
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator Ready()
+    {
+
+        yield break;
+    }
+
+
+    /// <summary>
+    /// ゲーム中の処理
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator OnGame()
+    {
+
+        yield break;
+    }
+
+
+    /// <summary>
+    /// ゲーム終了時の処理
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator End()
+    {
+        yield break;
+    }
+
+
+    /// <summary>
+    /// プレイヤー作成処理
+    /// </summary>
+    void CreatePlayer()
+    {
+
+    }
+
+}
