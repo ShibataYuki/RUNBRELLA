f2240a3cdfdb4254907cc78d0c84be34f1235e43
Nagisa-Ogawa <54796956+Nagisa-Ogawa@users.noreply.github.com>
2020-02-26 16:54:53 +0900

    [add] Add script PlayerJump.cs (#4)
    プレイヤーのジャンプ処理を追加しました。
    
    Co-authored-by: KUNISHIMAHAYATO <53422696+KUNISHIMAHAYATO@users.noreply.github.com>

---
+++ Unity Project/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    // ジャンプ力
+    [SerializeField]
+    private Vector2 jump = new Vector2(0.0f, 4.0f);
+    // 必要なコンポーネント
+    new Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // コンポーネントを取得
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    /// <summary>
+    /// ジャンプ処理
+    /// </summary>
+    public void Jump()
+    {
+        rigidbody.AddForce(jump, ForceMode2D.Impulse);
+    }
+}
