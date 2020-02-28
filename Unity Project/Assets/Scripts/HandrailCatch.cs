ae8d3421cb70302b26b7a1f852fb5a0357f91731
KUNISHIMAHAYATO <53422696+KUNISHIMAHAYATO@users.noreply.github.com>
2020-02-28 14:39:03 +0900

    Squashed commit of the following: (#8)
    commit f33ceab057571277c0a0ddb2e881f0de0d9bd294
    Author: Hayato Kunishima <Hayato.Kunishima@gmail.com>
    Date:   Fri Feb 28 14:34:23 2020 +0900
    
        [add] Add script HandrailCatch.cs
    
        手すりを掴むスクリプトを作成しました

---
+++ Unity Project/Assets/Scripts/HandrailCatch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandrailCatch : MonoBehaviour
+{
+    // 傘の柄
+    private GameObject grip = null;
+    // グリップの判定
+    private Vector2 gripPoint;
+    private float gripRadius;
+
+    // 手すりのレイヤー
+    [SerializeField]
+    private LayerMask handrail = 0;
+    // 手すりの上の空間のレイヤー
+    [SerializeField]
+    private LayerMask handrailArea = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 傘の柄を探す
+        grip = transform.Find("Grip").gameObject;
+        // コライダーを取得
+        var gripCollider = grip.GetComponent<CircleCollider2D>();
+        // 当たり判定の領域を計算
+        gripPoint  = gripCollider.offset;
+        gripRadius = gripCollider.radius;
+        // グリップを非表示にする
+        grip.SetActive(false);
+    }
+}
