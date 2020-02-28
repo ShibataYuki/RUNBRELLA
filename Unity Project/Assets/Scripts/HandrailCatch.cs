586a4eeabac6bb44c125a1e3808030abb32ff083
Nagisa-Ogawa <54796956+Nagisa-Ogawa@users.noreply.github.com>
2020-02-28 15:41:06 +0900

    [add] Add method HandrailCheck (#10)
    手すりを摑めるかチェックするメソッドを追加しました。
    
    Co-authored-by: KUNISHIMAHAYATO <53422696+KUNISHIMAHAYATO@users.noreply.github.com>

--- Unity Project/Assets/Scripts/HandrailCatch.cs
+++ Unity Project/Assets/Scripts/HandrailCatch.cs
@@ -8,6 +8,7 @@ public class HandrailCatch : MonoBehaviour
     private GameObject grip = null;
     // グリップの判定
     private Vector2 gripPoint;
+    // グリップの半径
     private float gripRadius;
 
     // 手すりのレイヤー
@@ -29,5 +30,25 @@ public class HandrailCatch : MonoBehaviour
         gripRadius = gripCollider.radius;
         // グリップを非表示にする
         grip.SetActive(false);
+    }
+
+    /// <summary>
+    /// 手すりの上にいるか、また傘の柄が手すりにかかってないかチェック
+    /// </summary>
+    /// <returns></returns>
+    public bool HandrailCheck()
+    {
+        // 現在の手すりの位置
+        var nowGripPoint = gripPoint + (Vector2)transform.position;
+        // 手すりの上にいるか、チェックする
+        if (Physics2D.OverlapCircle(nowGripPoint, gripRadius, handrail))
+        {
+            // 手すりにかかってないか、チェックする
+            if (!Physics2D.OverlapCircle(nowGripPoint, gripRadius, handrailArea))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
