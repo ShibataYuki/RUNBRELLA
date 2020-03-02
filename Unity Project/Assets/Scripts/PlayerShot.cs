6a3b75fb4e34a2d1cbb98c7f3adcf9c49d81abac
Nagisa-Ogawa <54796956+Nagisa-Ogawa@users.noreply.github.com>
2020-03-02 17:21:50 +0900

    [update]update_PlayerShot (#34)
    ・弾の数を追加しました

--- Unity Project/Assets/Scripts/PlayerShot.cs
+++ Unity Project/Assets/Scripts/PlayerShot.cs
@@ -6,7 +6,11 @@ public class PlayerShot : MonoBehaviour
 {
 
     BulletFactory bulletFactory;
-
+    [SerializeField]
+    private int bulletCount=3;
+    private int nowBulletCount = 0;
+    [SerializeField]
+    private float bulletChargeTime = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -17,13 +21,35 @@ public class PlayerShot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ChargeBullet();
     }
 
 
+    /// <summary>
+    /// 弾を発射する関数です
+    /// </summary>
+    /// <param name="position"></param>
     public void Shot(Vector2 position)
     {
-        bulletFactory.ShotBullet(position);
+        if(nowBulletCount>0)
+        {
+            nowBulletCount--;
+            bulletFactory.ShotBullet(position);
+        }
+    }
+
+
+    public void ChargeBullet()
+    {
+        // 一定時間経過で弾をチャージ
+        if(SceneManager.Instance.TimeCounter(bulletChargeTime))
+        {
+            nowBulletCount++;
+            if(nowBulletCount>bulletCount)
+            {
+                nowBulletCount = 3;
+            }
+        }
     }
 
 }
