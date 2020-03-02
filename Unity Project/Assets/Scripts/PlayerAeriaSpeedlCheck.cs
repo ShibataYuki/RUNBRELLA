62a31e2f66e48d65ee865b2c044a83a47844a08c
KUNISHIMAHAYATO <53422696+KUNISHIMAHAYATO@users.noreply.github.com>
2020-03-02 15:32:49 +0900

    [add] Add script PlayerAeriaSpeedlCheck.cs (#29)
    空中状態のプレイヤーの速度に最低保証を設定

---
+++ Unity Project/Assets/Scripts/PlayerAeriaSpeedlCheck.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 空中状態のときにスピードをチェックして止まらないようにする
+/// </summary>
+public class PlayerAeriaSpeedlCheck : MonoBehaviour
+{
+    // リジッドボディのコンポーネント
+    private new Rigidbody2D rigidbody;
+    // 最低速度
+    [SerializeField]
+    private float minSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // コンポーネントの取得
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    /// <summary>
+    /// 最低速度を下回っているかチェックして
+    /// 遅かったら最低速度に変更
+    /// </summary>
+    public void SpeedCheck()
+    {
+        // 速度の取得
+        var velocity = rigidbody.velocity;
+        // 横方向の移動量が最低速度以下なら
+        if (Mathf.Abs(velocity.x) < Mathf.Abs(minSpeed))
+        {
+            // 横方向の移動量を最低速度に変更
+            velocity.x = minSpeed;
+            rigidbody.velocity = velocity;
+        }
+    }
+}
