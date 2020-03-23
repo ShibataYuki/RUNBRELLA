ce264c664d551789000c939e34b96fd82979ebac
KUNISHIMAHAYATO <53422696+KUNISHIMAHAYATO@users.noreply.github.com>
2020-03-23 14:24:44 +0900

    Feature/update select stage (#132)
    * [update] Add Input in ProjectSettings.
    
    プロジェクトセッティングのインプットにキー情報、スティック情報を追加しました。
    
    * [add] Add new script for SelectStage.unity.
    
    セレクトステージ用にスクリプトを追加しました。
    
    * [update] Update SceneController for SelectStage.unity.
    
    セレクトステージ用のシーンコントローラーを更新しました。
    
    * [update] Update scene SelectStage.unity.
    
    セレクトステージを更新しました。

--- Unity Project/Assets/Scripts/OtherScene/SelectStage/SceneController.cs
+++ Unity Project/Assets/Scripts/OtherScene/SelectStage/SceneController.cs
@@ -1,22 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 namespace SelectStage
 {
     public class SceneController : MonoBehaviour
     {
+        #region シングルトンインスタンス
+
+        // インスタンスなアクセスポイント
+        private static SceneController instance = null;
+        public static SceneController Instance { get { return instance; } }
+
+        public void Awake()
+        {
+            // インスタンスが出来てなければ
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                // このコンポーネントがついたオブジェクトを消去する
+                Destroy(this.gameObject);
+            }
+        }
+        #endregion
+        // 必要なコンポーネント
+        private SelectPlayerNumberComponent selectPlayerNumberComponent;
+        private SelectCharacterComponent selectCharacterComponent;
+        // コンポーネントのアクセスポイント
+        public SelectPlayerNumberComponent SelectPlayerNumberComponent { get { return selectPlayerNumberComponent; } }
+        public SelectCharacterComponent SelectCharacterComponent { get { return selectCharacterComponent; } }
+
         // Start is called before the first frame update
         void Start()
         {
-
+            // コンポーネントの取得
+            selectPlayerNumberComponent = GetComponent<SelectPlayerNumberComponent>();
+            selectCharacterComponent = GetComponent<SelectCharacterComponent>();
+            // プレイ人数選択処理を開始する
+            StartCoroutine(selectPlayerNumberComponent.SelectPlayerNumber());
         }
 
-        // Update is called once per frame
-        void Update()
+        /// <summary>
+        /// シーンの読み込み処理
+        /// </summary>
+        /// <param name="sceneName">読み込むシーンの名前</param>
+        public void LoadScene(string sceneName)
         {
-
+            SceneManager.LoadScene(sceneName);
         }
+
+
     }
 }
---
+++ Unity Project/Assets/Scripts/OtherScene/SelectStage/SelectCharacterComponent.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SelectStage
+{
+    public class SelectCharacterComponent : MonoBehaviour
+    {
+        // プレイ人数
+        int playerNumber;
+
+        // 前のフレームにキー入力を行っているかどうかのフラグ
+        Dictionary <int, bool> keyFlag = new Dictionary<int, bool>();
+        // 決定したかどうかのフラグ
+        Dictionary<int, bool> isSubmits = new Dictionary<int, bool>();
+        // 選んだキャラクター
+        Dictionary<int, int> isSelect = new Dictionary<int, int>();
+
+        [SerializeField]
+        private GameObject[] charImages = null;
+
+        /// <summary>
+        /// フレーム更新処理を行うにあたっての初期化
+        /// </summary>
+        private void Init()
+        {
+            // プレイ人数の設定
+            playerNumber = GameManager.Instance.playerNumber;
+
+            // ディクショナリーの初期化
+            for (int i = 1; i <= playerNumber; i++)
+            {
+                keyFlag.Add(i, false);
+                isSubmits.Add(i, false);
+                isSelect.Add(i, 0);
+            }
+
+        }
+
+        /// <summary>
+        /// キャラクター選択画面
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator SelectCharacter()
+        {
+            // フレーム更新処理を行うにあたっての初期化
+            Init();
+
+            // 時間経過を待つ
+            yield return new WaitForSeconds(1.0f);
+
+            while (true)
+            {
+                // キー入力をチェックする
+                KeyIn();
+
+                // 全員の入力が終わっていれば
+                if (SubmitCheck() == true)
+                {
+                    // ステージに遷る
+                    SceneController.Instance.LoadScene("Stage");
+                    yield break;
+                }
+
+                yield return null;
+            } // while
+        } // IEnumerator
+
+        /// <summary>
+        /// キー入力をチェックするメソッド
+        /// </summary>
+        private void KeyIn()
+        {
+            // 全員のキー入力を見る
+            for (int i = 1; i <= playerNumber; i++)
+            {
+                // Aボタンを押したなら
+                if (Input.GetButtonDown("Player" + i.ToString() + "Submit"))
+                {
+                    // キャラクター選択を完了
+                    isSubmits[i] = true;
+                }
+                // Bボタンを押したなら
+                else if (Input.GetButtonDown("Player" + i.ToString() + "Cancel"))
+                {
+                    // キャラクターを選んでいたなら
+                    if (isSubmits[i] == true)
+                    {
+                        // キャラクターを選びなおす
+                        isSubmits[i] = false;
+                    }
+                    else
+                    {
+                        // プレイ人数選択画面に戻る
+                        BackSelectPlayerNumber();
+                        return;
+                    }
+                }
+                // 左右移動
+                var horizontal = Input.GetAxis("Player" + i.ToString() + "Horizontal");
+                if (Mathf.Abs(horizontal) > 0.7f)
+                {
+                    if (keyFlag[i] == false)
+                    {
+                        keyFlag[i] = true;
+
+                        if (horizontal > 0.0f)
+                        {
+                            isSelect[i]++;
+                        }
+
+                        else if (horizontal < 0.0f)
+                        {
+                            isSelect[i]--;
+                        }
+
+                        // 範囲内に収まるように
+                        isSelect[i] = Mathf.Clamp(isSelect[i], 0, charImages.Length - 1);
+
+                        Debug.Log(isSelect[i]);
+                    }
+                }
+                else
+                {
+                    keyFlag[i] = false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// プレイ人数選択画面に戻る
+        /// </summary>
+        private void BackSelectPlayerNumber()
+        {
+            // ディクショナリーのリセット
+            for (int i = 1; i <= playerNumber; i++)
+            {
+                keyFlag.Remove(i);
+                isSubmits.Remove(i);
+                isSelect.Remove(i);
+            }
+
+            // 人数選択画面に戻る
+            StartCoroutine(SceneController.Instance.SelectPlayerNumberComponent.SelectPlayerNumber());
+            // キャラ選択画面を停止させる
+            StopCoroutine(SelectCharacter());
+        }
+
+        /// <summary>
+        /// 全員の入力が終わったかチェックするメソッド
+        /// </summary>
+        /// <returns></returns>
+        private bool SubmitCheck()
+        {
+            // 全員のキャラ選択が終わったかチェック
+            for (int i = 1; i <= playerNumber; i++)
+            {
+                // 一人でも入力中の人がいれば
+                if (isSubmits[i] == false)
+                {
+                    // 入力を待つ
+                    return false;
+                }
+            }
+
+            // 入力の完了
+            return true;
+        }
+    }
+}
