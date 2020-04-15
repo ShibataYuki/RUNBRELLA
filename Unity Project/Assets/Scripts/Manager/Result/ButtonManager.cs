a65804a43f683b7a702d15ad2b3fa183ebfd0da6
KUNISHIMAHAYATO <53422696+KUNISHIMAHAYATO@users.noreply.github.com>
2020-04-15 19:38:46 +0900

    [update] Update Result for use button and selectScene. (#190)
    ・リザルト画面から遷移するシーンを選択できるようにしました。
    ・ボタン表示を追加し、それを使うように変更しました。

---
+++ Unity Project/Assets/Scripts/Manager/Result/ButtonManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Result
+{
+    public class ButtonManager : MonoBehaviour
+    {
+        // 現在選んでいるシーンのボタン
+        Dictionary<SceneController.SelectScene, GameObject> selectSceneButtons = new Dictionary<SceneController.SelectScene, GameObject>();
+        // 拡大縮小するコンポーネント
+        ScalingAnimation scalingAnimation;
+        // ボタンの親オブジェクト
+        GameObject buttons;
+        // ボタンの出現時の拡大する速度
+        private float scaleUpSpeedButtons = 2.0f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // 拡大縮小するスクリプトをコンポーネントしているオブジェクト
+            var scalingAnimationObject = GameObject.Find("ScalingAnimation");
+            // コンポーネントの取得
+            scalingAnimation = scalingAnimationObject.GetComponent<ScalingAnimation>();
+            // ボタンの親オブジェクト
+            buttons = GameObject.Find("Canvas/Buttons");
+            buttons.transform.localScale = Vector3.zero;
+            for (var scene = SceneController.SelectScene.Title; scene <= SceneController.SelectScene.SelectMenu; scene++)
+            {
+                // 拡大縮小するボタンのオブジェクト
+                var button = buttons.transform.Find(scene.ToString()).gameObject;
+                // ディクショナリーに追加
+                selectSceneButtons.Add(scene, button);
+            }
+        }
+
+        /// <summary>
+        /// ボタンの拡大縮小を行う
+        /// </summary>
+        /// <param name="selectScene">現在選んでいる次に読み込むシーン</param>
+        public void ScalingUpdate(SceneController.SelectScene selectScene)
+        {
+            // ボタンの親オブジェクトのスケールが1倍でなければ
+            if(buttons.transform.localScale != Vector3.one)
+            {
+                // ボタンの親オブジェクトを拡大する
+                ButtonsScaleUp();
+            }
+
+            foreach(var selectSceneButton in selectSceneButtons)
+            {
+                // 選ばれているシーンのボタンならScalingAnimationにセット
+                if (selectSceneButton.Key == selectScene)
+                {
+                    scalingAnimation.SetScalingObject(selectSceneButton.Value);
+                }
+                // 選ばれてないならスケールを0にする
+                else
+                {
+                    selectSceneButton.Value.transform.localScale = Vector3.one;
+                }
+            }
+        }
+
+        /// <summary>
+        /// ボタンの親オブジェクトを拡大する
+        /// </summary>
+        private void ButtonsScaleUp()
+        {
+            // 現在のスケール
+            var scale = buttons.transform.localScale;
+            // スケールに足す
+            scale.x += scaleUpSpeedButtons * Time.deltaTime;
+            // 0から1の間に収める
+            scale.x = Mathf.Clamp(scale.x, 0.0f, 1.0f);
+            // スケールに足す
+            scale.y += scaleUpSpeedButtons * Time.deltaTime;
+            // 0から1の間に収める
+            scale.y = Mathf.Clamp(scale.y, 0.0f, 1.0f);
+            // スケールに足す
+            scale.z += scaleUpSpeedButtons * Time.deltaTime;
+            // 0から1の間に収める
+            scale.z = Mathf.Clamp(scale.z, 0.0f, 1.0f);
+            // スケールをセット
+            buttons.transform.localScale = scale;
+        }
+    }
+}
--- Unity Project/Assets/Scripts/OtherScene/Result/SceneController.cs
+++ Unity Project/Assets/Scripts/OtherScene/Result/SceneController.cs
@@ -9,13 +9,30 @@ namespace Result
 {
     public class SceneController : MonoBehaviour
     {
+        // 選んでいるシーンのenum
+        public enum SelectScene
+        {
+            Title,
+            SelectMenu,
+        }
+        // 現在選んでいるシーン
+        private SelectScene selectScene = SelectScene.Title;
+
         PlayableDirector director;
         GameObject TimelineControllerObj;
+
+        // 選んでいるシーンのボタンのマネージャー
+        ButtonManager buttonManager;
         // Start is called before the first frame update
         void Start()
         {
             TimelineControllerObj = GameObject.Find("TimelineController");
+            // コンポーネントの取得
             director = TimelineControllerObj.GetComponent<PlayableDirector>();
+            // マネージャーのオブジェクト
+            var buttonManagerObject = GameObject.Find("ButtonManager");
+            // コンポーネントの取得
+            buttonManager = buttonManagerObject.GetComponent<ButtonManager>();
         }
 
         // Update is called once per frame
@@ -41,12 +58,47 @@ namespace Result
         {
             bool isTimelinePlaying = director.state == PlayState.Playing;
             if (isTimelinePlaying) { return; }
+            // 左右入力をチェックするコンポーネント
+            KeyCheckHorizontal();
+
+            // ボタンの拡大縮小
+            buttonManager.ScalingUpdate(selectScene);
             if (Input.GetKeyDown(KeyCode.Return) || GamePad.GetButtonDown(GamePad.Button.A, GamePad.Index.Any))
             {
-                // キャラ選択に戻る
-                SceneManager.LoadScene("SelectMenu");
+                // 選択したシーンに遷移
+                SceneManager.LoadScene(selectScene.ToString());
             }
         }
 
+        /// <summary>
+        /// 左右入力をチェックするコンポーネント
+        /// </summary>
+        void KeyCheckHorizontal()
+        {
+            // 左に移動させる入力を受け付けたかどうかのフラグ
+            var isLeft =  (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) || 
+                GamePad.GetButtonDown(GamePad.Button.LeftShoulder, GamePad.Index.Any));
+            // 右に移動させる入力を受け付けたかどうかのフラグ
+            var isRight = (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) ||
+                GamePad.GetButtonDown(GamePad.Button.RightShoulder, GamePad.Index.Any));
+
+            // 右への移動と左への移動のどちらか片方だけをうけたなら
+            if(isLeft != isRight)
+            {
+                // 左への移動
+                if(isLeft == true)
+                {
+                    selectScene--;
+                }
+                // 右への移動
+                else if(isRight == true)
+                {
+                    selectScene++;
+                }
+                // タイトルとリザルトの間に収める
+                selectScene = (SelectScene)Mathf.Clamp((int)selectScene, (int)SelectScene.Title, (int)SelectScene.SelectMenu);
+            }
+
+        }
     }
 }
