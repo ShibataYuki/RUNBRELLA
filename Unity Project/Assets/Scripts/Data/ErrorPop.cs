9cb816ac81ea1cab4c1568be0bf617869046b8d2
KUNISHIMAHAYATO <53422696+KUNISHIMAHAYATO@users.noreply.github.com>
2020-05-05 14:56:25 +0900

    [add] Add Spread Sheet Reader (#249)
    ・　グーグルスプレッドシートからデータを読み込み、テキストに変換し、さらにディクショナリにデータをまとめるクラスを追加しました
    ・　スプレッドシートのデータ記入方式に間違いがあった際に警告を出すポップアップウィンドウオブジェクトを追加し、プレファブ化しました
    ・　テキストデータをまとめるフォルダを追加しました
    
    Co-authored-by: Yuki Shibata <yuki.shibata58@gmail.com>

---
+++ Unity Project/Assets/Scripts/Data/ErrorPop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ErrorPop : MonoBehaviour
+{
+    // テキスト
+    [SerializeField]
+    Text text = null;
+    // ボタン
+    [SerializeField]
+    Button button = null;
+   
+    /// <summary>
+    /// ゲームを終了させる処理
+    /// </summary>
+    public void QuitGame()         // UIでボタンを押したときに呼ばれる
+    {
+        Debug.Log("aaa");
+        UnityEngine.Application.Quit();
+    }
+  
+    public void ChangeText(string text)
+    {
+        this.text.text = text;
+    }
+}
