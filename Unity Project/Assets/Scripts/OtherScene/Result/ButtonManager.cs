266ded824e52bb5d9312780930cde4487998f492
KUNISHIMAHAYATO <53422696+KUNISHIMAHAYATO@users.noreply.github.com>
2020-05-13 15:28:32 +0900

    Feature/update result/button manager (#272)
    * [add] Add sprite UI for Result button.
    
    リザルト用のボタンの画像を追加しました。
    
    * [add] Add some prefabs for Result.
    
    リザルト用のプレファブを追加しました。
    
    * [update] Update Scene Result for use some prefabs.
    
    シーン上のオブジェクトをプレファブに変更しました。
    
    * [add] Add some animations for Result UI.
    
    リザルトのUI用のアニメーションを追加しました。
    
    * [update] Update prefab Buttons in Result.
    
    リザルト用のボタンのプレファブを更新しました。
    ・アニメーターを追加しました。
    ・テキストを削除しました。
    ・画像の変更に合わせて子オブジェクトのサイズを変更しました。
    
    * [update] Update script ButtonManager for Result.
    
    リザルトのボタンを管理するマネージャーを変更しました。
    ・各ボタンの拡大縮小を行わなくしました。
    ・アニメーターを使用するように変更しました。

--- Unity Project/Assets/Scripts/OtherScene/Result/ButtonManager.cs
+++ Unity Project/Assets/Scripts/OtherScene/Result/ButtonManager.cs
@@ -7,24 +7,15 @@ namespace Result
     public class ButtonManager : MonoBehaviour
     {
         // 現在選んでいるシーンのボタン
-        Dictionary<SceneController.SelectScene, GameObject> selectSceneButtons = new Dictionary<SceneController.SelectScene, GameObject>();
-        // 拡大縮小するコンポーネント
-        ScalingAnimation scalingAnimation;
+        Dictionary<SceneController.SelectScene, Animator> selectSceneButtonsAnimator = new Dictionary<SceneController.SelectScene, Animator>();
+
+        private readonly int selectID = Animator.StringToHash("IsSelected");
         // ボタンの親オブジェクト
         GameObject buttons;
         // ボタンの出現時の拡大する速度
         private float scaleUpSpeedButtons = 2.0f;
         // スケーリング中かどうかのフラグ
         private bool isScalingButton = false;
-        // ボタンのスケーリング
-        // 選ばれていない場合のスケールの倍率
-        Vector3 defaultScale = Vector3.one * 0.5f;
-        // 選ばれているボタンのスケールの最大
-        float maxScale = 1;
-        // 選ばれているボタンのスケールの最小
-        float minScale = 0.25f;
-        // スケールを変更するスピード
-        float scaleUpSpeed = 0.75f;
         public bool IsScalingButton { get { return isScalingButton; } }
 
         // Start is called before the first frame update
@@ -32,26 +23,8 @@ namespace Result
         {
             // 各種パラメータをメンバー変数にセット
             SetParameter();
-            // 拡大縮小のパラメータをセット
-            InitScalingAnimation();
             // ボタンのディクショナリーにセット
             SetButtonDictionary();
-        }
-
-        /// <summary>
-        /// 拡大縮小するコンポーネントに各種パラメータをセット
-        /// </summary>
-        private void InitScalingAnimation()
-        {
-            // 拡大縮小するスクリプトをコンポーネントしているオブジェクト
-            var scalingAnimationObject = GameObject.Find("ScalingAnimation");
-            // コンポーネントの取得
-            scalingAnimation = scalingAnimationObject.GetComponent<ScalingAnimation>();
-            // 拡大率をセット
-            scalingAnimation.MaxScale = maxScale;
-            scalingAnimation.MinScale = minScale;
-			// 拡大縮小を行うスピードをセット
-            scalingAnimation.ScaleUpSpeed = scaleUpSpeed;
         }
 
         /// <summary>
@@ -66,8 +39,10 @@ namespace Result
             {
                 // 拡大縮小するボタンのオブジェクト
                 var button = buttons.transform.Find(scene.ToString()).gameObject;
+                // ボタンのアニメーターを手に入れる
+                var buttonAnimator = button.GetComponent<Animator>();
                 // ディクショナリーに追加
-                selectSceneButtons.Add(scene, button);
+                selectSceneButtonsAnimator.Add(scene, buttonAnimator);
             }
         }
 
@@ -85,11 +60,6 @@ namespace Result
                 try
                 {
                     // ディクショナリ－からパラメータを取得してセット
-                    maxScale = resultDictionary["ボタンの大きさが最大時のスケールの倍率"];
-                    minScale = resultDictionary["ボタンの大きさが最小時のスケールの倍率"];
-                    defaultScale = Vector3.one *
-                        resultDictionary["選ばれていないボタンのスケールの倍率"];
-                    scaleUpSpeed = 1f / resultDictionary["選ばれているボタンの拡大処理を行う秒数"];
                     scaleUpSpeedButtons = 1f / resultDictionary["ボタンが拡大しながら現れるのにかかる秒数"];
 
                 } // try
@@ -106,23 +76,15 @@ namespace Result
         }
 
         /// <summary>
-        /// ボタンの拡大縮小を行う
+        /// インデックスに応じてアニメーションで画像を変更する
         /// </summary>
         /// <param name="selectScene">現在選んでいる次に読み込むシーン</param>
-        public void ScalingUpdate(SceneController.SelectScene selectScene)
+        public void SetAnimator(SceneController.SelectScene selectScene)
         {
-            foreach(var selectSceneButton in selectSceneButtons)
+            foreach(var selectSceneButtonAnimator in selectSceneButtonsAnimator)
             {
-                // 選ばれているシーンのボタンならScalingAnimationにセット
-                if (selectSceneButton.Key == selectScene)
-                {
-                    scalingAnimation.SetScalingObject(selectSceneButton.Value);
-                }
-                // 選ばれてないならスケールを0にする
-                else
-                {
-                    selectSceneButton.Value.transform.localScale = defaultScale;
-                }
+                // 選ばれているシーンのボタンかどうかをアニメーターにセット
+                selectSceneButtonAnimator.Value.SetBool(selectID, selectSceneButtonAnimator.Key == selectScene);
             }
         }
 
