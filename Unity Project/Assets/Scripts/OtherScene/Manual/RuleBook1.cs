f7b353c975a3cacb5af77731f09f0b5bf9d7e0b8
Nagisa-Ogawa <54796956+Nagisa-Ogawa@users.noreply.github.com>
2020-06-06 15:14:55 +0900

    Feature/change manual sprite (#323)
    * [change] Change sprite Title.png for new version.
    
    新しいタイトル画像に差し替えました。
    
    * [change] Change manual sprite.
    
    ルール説明の画像を差し替えました。
    
    Co-authored-by: Hayato Kunishima <Hayato.Kunishima@gmail.com>

--- Unity Project/Assets/Scripts/OtherScene/Manual/RuleBook1.cs
+++ Unity Project/Assets/Scripts/OtherScene/Manual/RuleBook1.cs
@@ -7,16 +7,9 @@ namespace Manual
 {
     public class RuleBook1 : RuleBook
     {
-        Dictionary<string, MovieCotroller> movieControllerDictionary = new Dictionary<string, MovieCotroller>();
-        private readonly string slideObjectName = "SlideMovie";
-
         // Start is called before the first frame update
         void Start()
         {
-            var slideMovieObject = transform.Find(slideObjectName).gameObject;
-            var slideMovieController = slideMovieObject.GetComponent<MovieCotroller>();
-            movieControllerDictionary.Add(slideObjectName, slideMovieController);
-
         }
 
         public override void Entry()
@@ -26,25 +19,10 @@ namespace Manual
 
         public override void Do()
         {
-            if(GamePad.GetButtonDown(GamePad.Button.B, GamePad.Index.Any))
-            {
-                if(movieControllerDictionary[slideObjectName].IsMoviePlay == false)
-                {
-                    movieControllerDictionary[slideObjectName].MoviePlay();
-                }
-                else
-                {
-                    movieControllerDictionary[slideObjectName].MovieStop();
-                }
-            }
         }
 
         public override void Exit()
         {
-            foreach(var movieController in movieControllerDictionary.Values)
-            {
-                movieController.MovieStop();
-            }
         }
     }
 }
