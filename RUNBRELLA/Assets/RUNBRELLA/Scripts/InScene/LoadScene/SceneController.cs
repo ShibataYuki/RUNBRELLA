e8b97771db86a3d12bcc66919153be06efa62491
Yuki Shibata <yuki.shibata58@gmail.com>
2020-07-08 12:33:16 +0900

    [clean] Clean LoadScene Object in Hierarchy
    ・　「Load」シーンのヒエラルキーウィンドウ内のオブジェクトを整理しました。

--- RUNBRELLA/Assets/RUNBRELLA/Scripts/InScene/LoadScene/SceneController.cs
+++ RUNBRELLA/Assets/RUNBRELLA/Scripts/InScene/LoadScene/SceneController.cs
@@ -6,16 +6,11 @@ using UnityEngine.SceneManagement;
 namespace Load
 {
     public class SceneController : MonoBehaviour
-    {
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
+    {        
         // Update is called once per frame
         void Update()
         {
+            // スプレッドシートからのデータ読み込み完了を待ってシーン移行
             if(SheetToDictionary.Instance.IsCompletedSheetToText)
             {
                 SceneManager.LoadScene("Title");
